98bcb3332cd2a88ae90f7a7809b5a7a131da9d82
Ritesh Singh <123322562+Ritesh-977@users.noreply.github.com>
2026-07-06 12:31:04 +0530

    Streaming

---
+++ rendering-demo/app/components/product.tsx
@@ -0,0 +1,8 @@
+export const Product = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return (
+        <div>
+            <h1>Product</h1>
+        </div>
+    )
+}
